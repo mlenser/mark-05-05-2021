4ad454c872071515c8755452dae9c294cb916eb0
Mark Lenser <mlenser@gmail.com>
2021-05-04 22:11:13 +0200

    fix: more precision on the bars

--- src/utils/getPercentage.ts
+++ src/utils/getPercentage.ts
@@ -1,4 +1,4 @@
 import { roundToNearest } from './roundToNearest';
 
 export const getPercentage = (value: number) =>
-  `${roundToNearest({ interval: 1, value: value * 100 })}%`;
+  `${roundToNearest({ interval: 0.1, value: value * 100 })}%`;
