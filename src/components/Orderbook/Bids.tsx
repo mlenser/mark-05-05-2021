2253159286b267844bc5955f3a1aacf3b7aea5b1
Mark Lenser <mlenser@gmail.com>
2021-05-04 14:14:50 +0200

    feat: add running total

--- src/components/Orderbook/Bids.tsx
+++ src/components/Orderbook/Bids.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { sortByPriceHighFirst } from '../../utils/sort/sortByPrice';
 import { formatNumber } from '../../utils/formatNumber';
+import { addRunningTotal } from '../../utils/addRunningTotal';
 
 type Props = {
   values: SizePrice[];
 };
 
-const Bids: React.FC<Props> = ({ values }) => (
-  <div>
-    {values.sort(sortByPriceHighFirst).map(([price, size]) => (
-      <div>
-        {formatNumber(price)}: {formatNumber(size)}
-      </div>
-    ))}
-  </div>
-);
+const Bids: React.FC<Props> = ({ values }) => {
+  const sortedValues = values.sort(sortByPriceHighFirst);
+  const valuesWithTotal = sortedValues.reduce(addRunningTotal, []);
+  return (
+    <div>
+      {valuesWithTotal.map(([price, size, total]) => (
+        <div key={`${price}-${size}-${total}`}>
+          {formatNumber(price)}: {formatNumber(size)}: {formatNumber(total)}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Bids;
---
+++ src/utils/addRunningTotal.ts
@@ -0,0 +1,15 @@
+export const addRunningTotal = (
+  accumulator: SizePrice[],
+  currentValue: SizePrice,
+) => {
+  const currentSize = currentValue[1];
+  const lastItemInAccumulator = accumulator[accumulator.length - 1];
+  const previousTotal = Array.isArray(lastItemInAccumulator)
+    ? lastItemInAccumulator[2]
+    : 0;
+  const runningTotal = previousTotal + currentSize;
+
+  const newArray = [...currentValue, runningTotal];
+
+  return [...accumulator, newArray];
+};
