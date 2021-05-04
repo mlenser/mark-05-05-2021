0a73a575b6278b2b4e8b396c56c9c5e37f739e87
Mark Lenser <mlenser@gmail.com>
2021-05-04 12:31:47 +0200

    refactor: split DataWrapper from Orderbook

---
+++ src/components/Orderbook/DataWrapper.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useMemo, useRef } from 'react';
+import useWebSocket from 'react-use-websocket';
+import { connectionStatus } from '../../constants/websocket-states';
+
+const socketUrl = 'wss://www.cryptofacilities.com/ws/v1';
+
+const Orderbook: React.FC = () => {
+  const messageHistory = useRef([]);
+
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(
+    socketUrl,
+    {
+      // will attempt to reconnect on all close events, such as server shutting down
+      shouldReconnect: () => true,
+      reconnectAttempts: 10,
+      reconnectInterval: 3000,
+    },
+  );
+
+  messageHistory.current = useMemo(() => {
+    if (lastJsonMessage) {
+      return messageHistory.current.concat(lastJsonMessage);
+    }
+    return [];
+  }, [lastJsonMessage]);
+
+  useEffect(() => {
+    /*
+    sendJsonMessage({
+      event: 'subscribe',
+      feed: 'book_ui_1',
+      product_ids: ['PI_XBTUSD'],
+    });
+     */
+  }, []);
+
+  console.log('messageHistory.current', messageHistory.current);
+
+  return (
+    <div>
+      <div>Status: {connectionStatus[readyState]}.</div>
+      <ul>
+        {messageHistory.current?.map((message, idx) => {
+          return <li key={idx}>{JSON.stringify(message)}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Orderbook;
